724d1b7d50bd6a9415690aa6369a14cf98864f97
JoelPB <coffee.program.joel@gmail.com>
2021-04-30 17:11:21 -0300

    up to part10 delete

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -44,13 +44,23 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = verifyIfExists(id);
 //        Optional<Person> optionalPerson = personRepository.findById(id);
 //        if (optionalPerson.isEmpty()) {
 //            throw new PersonNotFoundException(id);
 //        }
 //        return personMapper.toDTO(optionalPerson.get());
         return personMapper.toDTO(person);
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
     }
 }
